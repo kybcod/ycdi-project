b9f41ca4dc14f95870bc5de125e2b06d62d525fb
김예빈 <kyb-0215@naver.com>
2025-02-24 10:19:19 +0900

    정적, mvc, api 간단 예제

--- src/main/java/com/example/project1/controller/HelloController.java
+++ src/main/java/com/example/project1/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.project1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,37 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Hello World");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // api : 객체가 넘어오면 JSON 형태로 데이터로 만들어서 데이터 반환(HttpMessageConverter)
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //{"name":"spring"}
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
