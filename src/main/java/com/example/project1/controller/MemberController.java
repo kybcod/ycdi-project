dd43d081640c605633b4699b0535493378ce7ace
김예빈 <kyb-0215@naver.com>
2025-02-25 15:38:08 +0900

    회원 등록 및 조회

--- src/main/java/com/example/project1/controller/MemberController.java
+++ src/main/java/com/example/project1/controller/MemberController.java
@@ -1,8 +1,15 @@
 package com.example.project1.controller;
 
+import com.example.project1.domain.Member;
 import com.example.project1.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -22,5 +29,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm"; // createMemberForm.html
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
