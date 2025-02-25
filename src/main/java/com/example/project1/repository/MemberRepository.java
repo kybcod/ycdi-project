3a76b930542bf58eae49b5d5b9f4ac9e672445d4
김예빈 <kyb-0215@naver.com>
2025-02-25 10:11:36 +0900

    리포지토리 테이트 케이스 작성

---
+++ src/main/java/com/example/project1/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.example.project1.repository;
+
+import com.example.project1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String username);
+    List<Member> findAll();
+}
+
+
