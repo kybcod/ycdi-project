a6380c786be1d2649a569f0245334ffdf67e6ca0
김예빈 <kyb-0215@naver.com>
2025-02-28 09:25:57 +0900

    순수 JDBC

--- src/main/java/com/example/project1/SpringConfig.java
+++ src/main/java/com/example/project1/SpringConfig.java
@@ -1,14 +1,21 @@
 package com.example.project1;
 
+import com.example.project1.repository.JdbcMemberRepository;
 import com.example.project1.repository.MemberRepository;
 import com.example.project1.repository.MemoryMemberRepository;
 import com.example.project1.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 컴포넌트 스캔을 사용하지 않고(@Service, @Repository) 직접 빈 설정
 @Configuration
 public class SpringConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
