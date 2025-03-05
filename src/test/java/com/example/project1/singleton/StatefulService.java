9ac328d73c0b9a0b1187a63caccaa6dfcb11858f
김예빈 <kyb-0215@naver.com>
2025-03-05 13:46:54 +0900

    스프링 빈은 무상태로

--- src/test/java/com/example/project1/singleton/StatefulService.java
+++ src/test/java/com/example/project1/singleton/StatefulService.java
@@ -4,9 +4,14 @@ public class StatefulService {
 
     private int price; // 상태를 유지하는 필드 : 10000 -> 20000
 
-    public void order(String name, int price){
+    /*public void order(String name, int price){
         System.out.println("name + \", price = \" + price = " + name + ", price = " + price);
         this.price = price; //여기가 문제!
+    }*/
+
+    public int order(String name, int price){
+        System.out.println("name + \", price = \" + price = " + name + ", price = " + price);
+        return price;
     }
 
     public int getPrice() {
--- src/test/java/com/example/project1/singleton/StatefulServiceTest.java
+++ src/test/java/com/example/project1/singleton/StatefulServiceTest.java
@@ -17,6 +17,8 @@ class StatefulServiceTest {
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
+        /*
+
         // ThreadA : A사용자 10000원 주문
         statefulService1.order("userA", 10000);
         // ThreadB : B사용자 120000원 주문
@@ -25,6 +27,13 @@ class StatefulServiceTest {
         // ThreadA : 사용자 주문 금액 조회
         int price = statefulService1.getPrice();
         System.out.println("price = " + price); //20000 : statefulService11,2 모두 같은 인스턴스이기 때문
+
+        */
+
+        /* 스프링 빈은 항상 무상태로 설계해야 함(지역변수 ...) */
+        int userA = statefulService1.order("userA", 10000);
+        int userB = statefulService1.order("userB", 20000);
+        System.out.println("price = " + userA); //10000
 
         assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
