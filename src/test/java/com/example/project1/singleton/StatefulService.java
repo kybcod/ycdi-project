e899451429a168c452966ac5d896c0f7df623054
김예빈 <kyb-0215@naver.com>
2025-03-05 13:44:36 +0900

    싱글톤 방식의 주의점

---
+++ src/test/java/com/example/project1/singleton/StatefulService.java
@@ -0,0 +1,15 @@
+package com.example.project1.singleton;
+
+public class StatefulService {
+
+    private int price; // 상태를 유지하는 필드 : 10000 -> 20000
+
+    public void order(String name, int price){
+        System.out.println("name + \", price = \" + price = " + name + ", price = " + price);
+        this.price = price; //여기가 문제!
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
