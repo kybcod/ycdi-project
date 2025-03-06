31c3ce8544fcd6a3f6da8c038a6dd00e6b2ac616
김예빈 <kyb-0215@naver.com>
2025-03-06 14:56:24 +0900

    빈 생명주기 InitializingBean, DisposableBean

---
+++ src/test/java/com/example/project1/lifecycle/NetworkClient.java
@@ -0,0 +1,41 @@
+package com.example.project1.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url =" + url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String msg) {
+        System.out.println("call : " + url + " msg : " + msg);
+    }
+
+    public void disconnect() {}
+
+    @Override
+    public void afterPropertiesSet(){
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+}
