ca5a1e324efce441203e3abb78f50e4d46fb8a44
김예빈 <kyb-0215@naver.com>
2025-03-07 10:57:48 +0900

    스코프와 Provider

--- src/main/java/com/example/project1/web/LogDemoController.java
+++ src/main/java/com/example/project1/web/LogDemoController.java
@@ -3,22 +3,23 @@ package com.example.project1.web;
 import com.example.project1.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.net.http.HttpRequest;
 
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
 
     private final LogDemonService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURI().toString();
         myLogger.setRequestURL(requestURL);
 
--- src/main/java/com/example/project1/web/LogDemonService.java
+++ src/main/java/com/example/project1/web/LogDemonService.java
@@ -2,15 +2,17 @@ package com.example.project1.web;
 
 import com.example.project1.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Scope;
 
 @Scope
 @RequiredArgsConstructor
 public class LogDemonService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = "+ id);
     }
 }
