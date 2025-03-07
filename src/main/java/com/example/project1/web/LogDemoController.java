071a5513167ec211390f82e0bfe34a86e42a8797
김예빈 <kyb-0215@naver.com>
2025-03-07 10:49:57 +0900

    request 스코프

---
+++ src/main/java/com/example/project1/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package com.example.project1.web;
+
+import com.example.project1.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.http.HttpRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemonService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
---
+++ src/main/java/com/example/project1/web/LogDemonService.java
@@ -0,0 +1,16 @@
+package com.example.project1.web;
+
+import com.example.project1.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Scope;
+
+@Scope
+@RequiredArgsConstructor
+public class LogDemonService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = "+ id);
+    }
+}
